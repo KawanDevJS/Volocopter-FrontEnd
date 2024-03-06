28e6ca537f940be34b5d33326963f38771063b4a
Kawan Nascimento <100882429+KawanDevJS@users.noreply.github.com>
2024-03-05 21:09:57 -0300

    Add files via upload

---
+++ contato/contato.js
@@ -0,0 +1,22 @@
+/* Todos os direitos reservados para Kawan Desenvolvedor Fullstack.2023*/
+/* GITHUB: @kawan3917
+// Trabalho Acadêmico - ANÁLISE E DESENVOLVIMENTO DE SISTEMAS - ITALO BRASILEIRO*/
+
+//header scroll
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
